62449ed63d88f68b9136e72287d41a83db979695
pew35 <peggywu226@gmail.com>
2024-12-07 05:42:43 -0800

    fix connection

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ import "dotenv/config";
 
 
 
-const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING //|| "mongodb://127.0.0.1:27017/kanbas"
 mongoose.connect(CONNECTION_STRING);
 
 const app = express();
